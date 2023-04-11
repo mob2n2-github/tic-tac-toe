020d4af5630f194e489bf8deebb571146151442c
mob2n2-github <130398997+mob2n2-github@users.noreply.github.com>
2023-04-11 09:15:30 -0400

    2 files modified

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
 import './style.css';
 
-export default function App() {
+export default function Square() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <button className="square">X</button>
+      <button className="square">X</button>
     </div>
   );
+
 }
